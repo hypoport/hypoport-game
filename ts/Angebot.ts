611a1ad1d8e4d2a4f04d0014d78ee8cc57cd3df5
Eric Karge <eric.karge@hypoport.de>
2014-12-05 13:07:34 +0100

    AngebotGenerator calculates Betrag

--- ts/Angebot.ts
+++ ts/Angebot.ts
@@ -8,20 +8,24 @@ module hypoport.game {
     export class Angebot {
         public zins:number;
         public bindung:number;
+        public betrag:number;
         public machbarkeit:Machbarkeit;
     }
 
     export class AngebotGenerator {
         static ZINS_BASE = 200;
-        static BINDUNG_BASE = 15;
         static ZINS_VARIANCE = 200;
+        static BINDUNG_BASE = 15;
         static  BINDUNG_VARIANCE = 15;
+        static BETRAG_BASE = 100000;
+        static  BETRAG_VARIANCE = 40000;
 
         public createRandomAngebot():Angebot {
             var angebot = new Angebot();
             angebot.machbarkeit = (<any>Machbarkeit)[Machbarkeit[Math.floor(3 * (Math.random()))]];
             angebot.zins = AngebotGenerator.createRandomInteger(AngebotGenerator.ZINS_BASE, AngebotGenerator.ZINS_VARIANCE) / 100;
             angebot.bindung = AngebotGenerator.createRandomInteger(AngebotGenerator.BINDUNG_BASE, AngebotGenerator.BINDUNG_VARIANCE);
+            angebot.betrag = AngebotGenerator.createRandomInteger(AngebotGenerator.BETRAG_BASE, AngebotGenerator.BETRAG_VARIANCE);
             return  angebot;
         }
 
